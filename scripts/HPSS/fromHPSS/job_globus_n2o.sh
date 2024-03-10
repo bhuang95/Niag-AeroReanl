94f8e20d3fa30fde175849b5d962721ccee9a602
Bo Huang <bohuang9955@gmail.com>
2024-03-10 19:32:50 +0000

    1st commit

---
+++ scripts/HPSS/fromHPSS/job_globus_n2o.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+#SBATCH -J glbus_h2n
+#SBATCH -A niagara
+#SBATCH -n 1
+#SBATCH -t 24:00:00
+#SBATCH -p service
+#SBATCH -D ./
+#SBATCH -o /collab1/data/Bo.Huang/dataTransfer/AeroReanl/ChgresGDAS/glbus_h2n.out
+#SBATCH -e /collab1/data/Bo.Huang/dataTransfer/AeroReanl/ChgresGDAS/glbus_h2n.out
+
+module load globus-cli
+
+set -x
+
+SDATE=$(cat SDATE.info)  #2020071300
+EDATE=$(cat EDATE.info)  #2020071318
+HRS="00 06 12 18"
+CYCINC=24
+NDATE="/home/Bo.Huang/Projects/AeroReanl/bin/ndate"
+CURDIR="/home/Bo.Huang/Projects/AeroReanl/scripts/HPSS/fromHPSS"
+
+NIAGEP=1bfd8a79-52b2-4589-88b2-0648e0c0b35d
+ORIONEP=2cf2c281-cafc-4b20-900b-45abeb042854
+
+CASE_GDAS="C96"
+CASE_ENKF="C96"
+NMEMSGRPS="01-40"
+GFSVER=v15
+NIAGDIR=/collab1/data/Bo.Huang/dataTransfer/AeroReanl/ChgresGDAS/chgres-${GFSVER}
+ORIONDIR=/work/noaa/rstprod/bohuang/DataTransfer/FromNiag/AeroReanl/ChgresGDAS/chgres-${GFSVER}
+RECORD=${CURDIR}/record_glbus_n2o.log
+LOGDIR=${NIAGDIR}/logs
+
+[[ ! -d ${LOGDIR} ]] && mkdir -p ${LOGDIR}
+
+CDATE=${SDATE}
+while [ ${CDATE} -le ${EDATE} ]; do
+    echo ${CDATE}
+    ICNT=0
+    CY=${CDATE:0:4}
+    CM=${CDATE:4:2}
+    CD=${CDATE:6:2}
+    CYMD=${CDATE:0:8}
+    #CH=${CDATE:8:2}
+    NIAGGDAS=${NIAGDIR}/GDAS_CHGRES_NC_${CASE_GDAS}/${CY}/${CY}${CM}/${CY}${CM}${CD}
+    NIAGENKF=${NIAGDIR}/ENKFGDAS_CHGRES_NC_${CASE_ENKF}/${CY}/${CY}${CM}/${CY}${CM}${CD}
+    ORIONGDAS=${ORIONDIR}/GDAS_CHGRES_NC_${CASE_GDAS}/${CY}/${CY}${CM}/${CY}${CM}${CD}
+    ORIONENKF=${ORIONDIR}/ENKFGDAS_CHGRES_NC_${CASE_ENKF}/${CY}/${CY}${CM}/${CY}${CM}${CD}
+
+    GINPUT=${LOGDIR}/GlobusInput.out
+    GID=${LOGDIR}/GlobusID_${CYMD}.out
+    [[ -f ${GINPUT} ]] && rm -rf ${GINPUT}
+    for CH in 00 06 12 18; do
+        NIAGFILE=${NIAGGDAS}/gdas.${CYMD}${CH}.${CASE_GDAS}.NC.tar
+        ORIONFILE=${ORIONGDAS}/gdas.${CYMD}${CH}.${CASE_GDAS}.NC.tar
+	echo "${NIAGFILE} ${ORIONFILE}" >> ${GINPUT}
+
+        NIAGFILE=${NIAGENKF}/enkfgdas.${CYMD}${CH}.${CASE_ENKF}.NC.${NMEMSGRPS}.tar
+        ORIONFILE=${ORIONENKF}/enkfgdas.${CYMD}${CH}.${CASE_ENKF}.NC.${NMEMSGRPS}.tar
+	echo "${NIAGFILE}    ${ORIONFILE}" >> ${GINPUT}
+    done 
+
+    #globus transfer ${NIAGEP}:/ ${ORIONEP}:/ --batch --label "CLI batch" < ${GINPUT} >& ${GID}
+    globus transfer --notify failed,inactive ${NIAGEP}:/ ${ORIONEP}:/  --batch ${GINPUT} >& ${GID}
+    ERR=$?
+    ICNT=$((${ICNT}+${ERR}))
+
+    GLBUSID=$(tail -n 1 ${GID} | awk '{print $3}')
+    globus task wait "${GLBUSID}"
+    ERR=$?
+    ICNT=$((${ICNT}+${ERR}))
+
+    if [ ${ICNT} -ne 0 ]; then
+        echo "Globus n2o failed at ${CDATE} and exit"
+	echo "${CDATE}: FAILED" >> ${RECORD}
+        exit ${ICNT}
+    else
+	echo "${CDATE}: SUCCESSFUL" >> ${RECORD}
+    fi
+    CDATE=$(${NDATE} ${CYCINC} ${CDATE})
+    ERR=$?
+    ICNT=$((${ICNT}+${ERR}))
+    if [ ${ICNT} -ne 0 ]; then
+        echo "Continuing to next cycle failed  at ${CDATE}"
+	exit ${ICNT}
+    fi
+done
+
+exit ${ICNT}
+
---
+++ scripts/HPSS/fromHPSS/job_ret_gdas.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+#SBATCH -J ret_h2n
+#SBATCH -A niagara
+#SBATCH -n 1
+#SBATCH -t 24:00:00
+#SBATCH -p service
+#SBATCH -D ./
+#SBATCH -o /collab1/data/Bo.Huang/dataTransfer/AeroReanl/ChgresGDAS/ret_h2n.out
+#SBATCH -e /collab1/data/Bo.Huang/dataTransfer/AeroReanl/ChgresGDAS/ret_h2n.out
+
+module load hpss
+
+set -x
+
+SDATE=2020072500
+EDATE=2020083118
+echo "${SDATE}" > SDATE.info 
+echo "${EDATE}" > EDATE.info 
+CYCINC=6
+NDATE="/home/Bo.Huang/Projects/AeroReanl/bin/ndate"
+CURDIR="/home/Bo.Huang/Projects/AeroReanl/scripts/HPSS/fromHPSS"
+
+CASE_GDAS="C96"
+CASE_ENKF="C96"
+NMEMSGRPS="01-40"
+GFSVER=v15
+HPSSDIR=/BMC/fim/5year/MAPP_2018/bhuang/BackupGdas/chgres-${GFSVER}
+NIAGDIR=/collab1/data/Bo.Huang/dataTransfer/AeroReanl/ChgresGDAS/chgres-${GFSVER}
+RECORD=${CURDIR}/record_gdas_h2n.log
+
+CDATE=${SDATE}
+while [ ${CDATE} -le ${EDATE} ]; do
+    echo ${CDATE}
+    ICNT=0
+    CY=${CDATE:0:4}
+    CM=${CDATE:4:2}
+    CD=${CDATE:6:2}
+    CH=${CDATE:8:2}
+    
+    HPSSGDAS=${HPSSDIR}/GDAS_CHGRES_NC_${CASE_GDAS}/${CY}/${CY}${CM}/${CY}${CM}${CD}
+    HPSSENKF=${HPSSDIR}/ENKFGDAS_CHGRES_NC_${CASE_ENKF}/${CY}/${CY}${CM}/${CY}${CM}${CD}
+    NIAGGDAS=${NIAGDIR}/GDAS_CHGRES_NC_${CASE_GDAS}/${CY}/${CY}${CM}/${CY}${CM}${CD}
+    NIAGENKF=${NIAGDIR}/ENKFGDAS_CHGRES_NC_${CASE_ENKF}/${CY}/${CY}${CM}/${CY}${CM}${CD}
+
+    [[ ! -d ${NIAGGDAS} ]] && mkdir -p ${NIAGGDAS}
+    [[ ! -d ${NIAGENKF} ]] && mkdir -p ${NIAGENKF}
+    cd ${NIAGGDAS}
+    HPSSFILE=${HPSSGDAS}/gdas.${CDATE}.${CASE_GDAS}.NC.tar
+    hsi get ${HPSSFILE}
+    ERR=$?
+    ICNT=$((${ICNT}+${ERR}))
+
+    cd ${NIAGENKF}
+    HPSSFILE=${HPSSENKF}/enkfgdas.${CDATE}.${CASE_ENKF}.NC.${NMEMSGRPS}.tar
+    hsi get ${HPSSFILE}
+    ERR=$?
+    ICNT=$((${ICNT}+${ERR}))
+    if [ ${ICNT} -ne 0 ]; then
+        echo "HSI GET failed at ${CDATE} and exit"
+	echo "${CDATE}: FAILED" >> ${RECORD}
+        exit ${ICNT}
+    else
+	echo "${CDATE}: SUCCESSFUL" >> ${RECORD}
+    fi
+    CDATE=$(${NDATE} ${CYCINC} ${CDATE})
+    ERR=$?
+    ICNT=$((${ICNT}+${ERR}))
+    if [ ${ICNT} -ne 0 ]; then
+        echo "Continuing to next cycle failed at ${CDATE}"
+	exit ${ICNT}
+    fi
+done
+
+cd ${CURDIR}
+/apps/slurm/default/bin/sbatch job_globus_n2o.sh
+ERR=$?
+ICNT=$((${ICNT}+${ERR}))
+if [ ${ICNT} -ne 0 ]; then
+    echo "Submittimg globus failed  at ${CDATE}"
+    exit ${ICNT}
+fi
+exit ${ICNT}
+
